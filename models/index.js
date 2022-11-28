60c38511c0e1786cef1f7db84533624cfad5c17c
Gareth Kwan <gareth0201@gmail.com>
2022-11-27 22:58:39 -0500

    Working on routes

--- models/index.js
+++ models/index.js
@@ -1,16 +1,31 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
+// const { FOREIGNKEYS } = require("sequelize/types/lib/query-types");
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: "Category.id",
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: "Category.id",
+});
 
 // Products belongToMany Tags (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: "ProductTag",
+  foreignKey: "Product.id",
+});
 
 // Tags belongToMany Products (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: "ProductTag",
+  foreignKey: "Product.id",
+});
 
 module.exports = {
   Product,
